2ae04ebb87b49a48158929138529c19ae192ded5
Neil Henning <neil.henning@hey.com>
2021-12-10 07:07:14 +0000

    Day 10 solutions.

--- src/day10.zig
+++ src/day10.zig
@@ -1,12 +1,5 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
 const print = std.debug.print;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
@@ -14,5 +7,102 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day10.txt");
 
 pub fn main() !void {
-    
+  var timer = try std.time.Timer.start();
+
+  var lines = std.mem.tokenize(data, "\r\n");
+
+  var chunks = std.ArrayList(u8).init(gpa);
+  defer { chunks.deinit(); }
+
+  var scores = std.ArrayList(u64).init(gpa);
+  defer { scores.deinit(); }
+
+  var total : u32 = 0;
+
+  while (lines.next()) |line| {
+    var syntaxError = false;
+
+    for (line) |c| {
+      switch (c) {
+        '(', '[', '{', '<' => try chunks.append(c),
+        ')' => {
+          const pop = chunks.popOrNull();
+          if (pop == null or pop.? != '(') {
+            total += 3;
+            syntaxError = true;
+            break;
+          }
+        },
+        ']' => {
+          const pop = chunks.popOrNull();
+          if (pop == null or pop.? != '[') {
+            total += 57;
+            syntaxError = true;
+            break;
+          }
+        },
+        '}' => {
+          const pop = chunks.popOrNull();
+          if (pop == null or pop.? != '{') {
+            total += 1197;
+            syntaxError = true;
+            break;
+          }
+        },
+        '>' => {
+          const pop = chunks.popOrNull();
+          if (pop == null or pop.? != '<') {
+            total += 25137;
+            syntaxError = true;
+            break;
+          }
+        },
+        else => unreachable,
+      }
+    }
+
+    // We need to skip any corrupted ones, and only score the incomplete ones.
+    if (syntaxError) {
+      // We're reusing chunks for each line check, so clear it for the next one!
+      chunks.clearRetainingCapacity();
+
+      continue;
+    }
+
+    var score : u64 = 0;
+
+    while (chunks.items.len != 0) {
+      score *= 5;
+      
+      switch (chunks.pop()) {
+        '(' => score += 1,
+        '[' => score += 2,
+        '{' => score += 3,
+        '<' => score += 4,
+        else => unreachable
+      }
+    }
+
+    try scores.append(score);
+  }
+
+  {
+    print("🎁 Syntax error: {}\n", .{total});
+    print("Day 10 - part 01 took {:12}ns\n", .{timer.lap()});
+    timer.reset();
+  }
+
+  std.sort.sort(u64, scores.items, {}, compare);
+
+  const middle = scores.items[scores.items.len / 2];
+
+  {
+    print("🎁 Middle score: {}\n", .{middle});
+    print("Day 10 - part 02 took {:12}ns\n", .{timer.lap()});
+    print("❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️\n", .{});
+  }
+}
+
+fn compare(context: void, a: u64, b: u64) bool {
+    return a < b;
 }
