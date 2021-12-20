fe924d66ce649a129c1701779bdc24e9e0817a7e
Neil Henning <neil.henning@hey.com>
2021-12-20 07:43:59 +0000

    day 20 solutions.

--- src/day19.zig
+++ src/day19.zig
@@ -1,12 +1,5 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
 const print = std.debug.print;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
@@ -14,5 +7,455 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day19.txt");
 
 pub fn main() !void {
-    
+  var timer = try std.time.Timer.start();
+  var scanners = try calculateScanners(data);
+  defer { deinitScanners(scanners); }
+  print("🎁 Magnitude: {}\n", .{try numberOfBeacons(scanners)});
+  print("Day 19 - part 01 took {:15}ns\n", .{timer.lap()});
+  timer.reset();
+
+
+  print("🎁 Largest magnitude: {}\n", .{maxManhattan(scanners)});
+  print("Day 19 - part 02 took {:15}ns\n", .{timer.lap()});
+  print("❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️\n", .{});
+}
+
+const Point = struct {
+  x : i32,
+  y : i32,
+  z : i32,
+
+  pub fn dump(me : *const @This()) void {
+    print("({}, {}, {})\n", .{me.x, me.y, me.z});
+  }
+
+  pub fn init(x : i32, y : i32, z : i32) @This() {
+    return Point {
+      .x = x,
+      .y = y,
+      .z = z
+    };
+  }
+
+  pub fn equals(me : *const @This(), other : @This()) bool {
+    return me.x == other.x and me.y == other.y and me.z == other.z;
+  }
+
+  pub fn sub(me : *const @This(), other : @This()) @This() {
+    return Point {
+      .x = me.x - other.x,
+      .y = me.y - other.y,
+      .z = me.z - other.z,
+    };
+  }
+
+  pub fn add(me : *const @This(), other : @This()) @This() {
+    return Point {
+      .x = me.x + other.x,
+      .y = me.y + other.y,
+      .z = me.z + other.z,
+    };
+  }
+
+  pub fn abs(me : *const @This()) @This() {
+    return Point {
+      .x = if (me.x < 0) -me.x else me.x,
+      .y = if (me.y < 0) -me.y else me.y,
+      .z = if (me.z < 0) -me.z else me.z,
+    };
+  }
+};
+
+const BeaconIterator = struct {
+  buffer : []const Point,
+  index : usize,
+  rotation : u6,
+  position : ?Point,
+
+  pub fn init(buffer : []const Point, rotation : u6, position : ?Point) @This() {
+    return BeaconIterator {
+      .buffer = buffer,
+      .index = 0,
+      .rotation = rotation,
+      .position = position,
+    };
+  }
+
+  fn rotate(a : *i32, b : *i32, rotation: u2) void {
+    const aOld = a.*;
+    const bOld = b.*;
+
+    switch (rotation) {
+      0 => {},
+      1 => {
+        // 90 degrees
+        a.* = bOld;
+        b.* = -aOld;
+      },
+      2 => {
+        // 180 degrees
+        a.* = -aOld;
+        b.* = -bOld;
+      },
+      3 => {
+        // 270 degrees
+        a.* = -bOld;
+        b.* = aOld;
+      }
+    }
+  }
+
+  pub fn next(me : *@This()) ?Point {
+    if (me.index == me.buffer.len) {
+      return null;
+    }
+
+    var point = me.buffer[me.index];
+
+    const xRotation = @intCast(u2, (me.rotation >> 0) & 0x3);
+    const yRotation = @intCast(u2, (me.rotation >> 2) & 0x3);
+    const zRotation = @intCast(u2, (me.rotation >> 4) & 0x3);
+
+    rotate(&point.x, &point.y, xRotation);
+    rotate(&point.y, &point.z, yRotation);
+    rotate(&point.x, &point.z, zRotation);
+
+    // Bump our index too!
+    me.index += 1;
+
+    if (me.position != null) {
+      point = point.add(me.position.?);
+    }
+
+    return point;
+  }
+};
+
+const Scanner = struct {
+  beacons : std.ArrayList(Point),
+  aligned : bool,
+  rotation : u6,
+  position : ?Point,
+
+  pub fn init(allocator : *std.mem.Allocator) @This() {
+    return Scanner {
+      .beacons = std.ArrayList(Point).init(gpa),
+      .aligned = false,
+      .rotation = 0,
+      .position = null,
+    };
+  }
+
+  pub fn deinit(me : @This()) void {
+    me.beacons.deinit();
+  }
+
+  pub fn add(me : *@This(), point : Point) !void {
+    try me.beacons.append(point);
+  }
+
+  pub fn fix(me : *@This(), rotation : u6, position : Point) void {
+    me.aligned = true;
+    me.rotation = rotation;
+    me.position = position;
+  }
+
+  pub fn fixedIterator(me : *const @This()) BeaconIterator {
+    std.debug.assert(me.aligned);
+    std.debug.assert(me.beacons.items.len != 0);
+    return BeaconIterator.init(me.beacons.items, me.rotation, me.position);
+  }
+
+  pub fn rotatedIterator(me : *const @This(), rotation : u6) BeaconIterator {
+    // We cannot rotate an already aligned scanner!
+    std.debug.assert(!me.aligned);
+    std.debug.assert(me.beacons.items.len != 0);
+    return BeaconIterator.init(me.beacons.items, rotation, null);
+  }
+};
+
+pub fn deinitScanners(scanners : std.ArrayList(Scanner)) void {
+  for (scanners.items) |scanner| {
+    scanner.deinit();
+  }
+  scanners.deinit();
+}
+
+pub fn calculateScanners(input : []const u8) !std.ArrayList(Scanner) {
+  var scanners = std.ArrayList(Scanner).init(gpa);
+
+  var splitIterator = std.mem.split(input, "--- scanner ");
+
+  while (splitIterator.next()) |split| {
+    var lines = std.mem.tokenize(split, "\r\n");
+
+    // Skip the first line that is just 'N ---'
+    const discard = lines.next();
+
+    var scanner = Scanner.init(gpa);
+
+    while (lines.next()) |line| {
+      var coordIterator = std.mem.tokenize(line, ",");
+
+      const x = try std.fmt.parseInt(i32, coordIterator.next().?, 10);
+      const y = try std.fmt.parseInt(i32, coordIterator.next().?, 10);
+      const z = try std.fmt.parseInt(i32, coordIterator.next().?, 10);
+
+      try scanner.add(Point.init(x, y, z));
+    }
+
+    if (scanner.beacons.items.len == 0) {
+      scanner.deinit();
+    } else {
+      try scanners.append(scanner);
+    }
+  }
+
+  // We are going to align all the scanners relative to scanner 0, so lock in
+  // the alignment of scanner 0 now.
+  scanners.items[0].fix(0, Point.init(0, 0, 0));
+
+  while (true) {
+    var anyUnaligned = false;
+
+    for (scanners.items) |*scanner, i| {
+      for (scanners.items) |other, k| {
+        if (scanner.aligned) {
+          break;
+        }
+
+        // Skip ourselves.
+        if (i == k) {
+          continue;
+        }
+
+        anyUnaligned = true;
+
+        // We need the other scanner to be aligned.
+        if (!other.aligned) {
+          continue;
+        }
+
+        var rotation : u6 = 0;
+
+        while (true) {
+          var fixedIterator = other.fixedIterator();
+          while (fixedIterator.next()) |fixed| {
+            if (scanner.aligned) {
+              break;
+            }
+
+            var rotatedIterator = scanner.rotatedIterator(rotation);
+            while (rotatedIterator.next()) |rotated| {
+              const diff = fixed.sub(rotated);
+
+              var fixedIterator2 = other.fixedIterator();
+              var matches : u32 = 0;
+              
+              while (fixedIterator2.next()) |fixed2| {
+                var rotatedIterator2 = scanner.rotatedIterator(rotation);
+                while (rotatedIterator2.next()) |rotated2| {
+                  if (fixed2.equals(rotated2.add(diff))) {
+                    matches += 1;
+                  }
+                }
+              }
+
+              if (matches >= 12) {
+                scanner.fix(rotation, diff);
+                break;
+              }
+            }
+          }
+
+          if (@addWithOverflow(u6, rotation, 1, &rotation)) {
+            break;
+          }
+        }
+      }
+    }
+
+    if (!anyUnaligned) {
+      break;
+    }
+  }
+
+  return scanners;
+}
+
+pub fn numberOfBeacons(scanners : std.ArrayList(Scanner)) !u32 {
+  var map = std.AutoHashMap(Point, void).init(gpa);
+  defer { map.deinit(); }
+
+  for (scanners.items) |scanner| {
+    var iterator = scanner.fixedIterator();
+
+    while (iterator.next()) |beacon| {
+      try map.put(beacon, .{});
+    }
+  }
+
+  return map.count();
+}
+
+pub fn maxManhattan(scanners : std.ArrayList(Scanner)) i32 {
+  var max : i32 = 0;
+
+  for (scanners.items) |i| {
+    for (scanners.items) |k| {
+      const vector = i.position.?.sub(k.position.?).abs();
+      const distance = vector.x + vector.y + vector.z;
+
+      max = std.math.max(max, distance);
+    }
+  }
+
+  return max;
+}
+
+test "example" {
+  const input = \\--- scanner 0 ---
+\\404,-588,-901
+\\528,-643,409
+\\-838,591,734
+\\390,-675,-793
+\\-537,-823,-458
+\\-485,-357,347
+\\-345,-311,381
+\\-661,-816,-575
+\\-876,649,763
+\\-618,-824,-621
+\\553,345,-567
+\\474,580,667
+\\-447,-329,318
+\\-584,868,-557
+\\544,-627,-890
+\\564,392,-477
+\\455,729,728
+\\-892,524,684
+\\-689,845,-530
+\\423,-701,434
+\\7,-33,-71
+\\630,319,-379
+\\443,580,662
+\\-789,900,-551
+\\459,-707,401
+\\
+\\--- scanner 1 ---
+\\686,422,578
+\\605,423,415
+\\515,917,-361
+\\-336,658,858
+\\95,138,22
+\\-476,619,847
+\\-340,-569,-846
+\\567,-361,727
+\\-460,603,-452
+\\669,-402,600
+\\729,430,532
+\\-500,-761,534
+\\-322,571,750
+\\-466,-666,-811
+\\-429,-592,574
+\\-355,545,-477
+\\703,-491,-529
+\\-328,-685,520
+\\413,935,-424
+\\-391,539,-444
+\\586,-435,557
+\\-364,-763,-893
+\\807,-499,-711
+\\755,-354,-619
+\\553,889,-390
+\\
+\\--- scanner 2 ---
+\\649,640,665
+\\682,-795,504
+\\-784,533,-524
+\\-644,584,-595
+\\-588,-843,648
+\\-30,6,44
+\\-674,560,763
+\\500,723,-460
+\\609,671,-379
+\\-555,-800,653
+\\-675,-892,-343
+\\697,-426,-610
+\\578,704,681
+\\493,664,-388
+\\-671,-858,530
+\\-667,343,800
+\\571,-461,-707
+\\-138,-166,112
+\\-889,563,-600
+\\646,-828,498
+\\640,759,510
+\\-630,509,768
+\\-681,-892,-333
+\\673,-379,-804
+\\-742,-814,-386
+\\577,-820,562
+\\
+\\--- scanner 3 ---
+\\-589,542,597
+\\605,-692,669
+\\-500,565,-823
+\\-660,373,557
+\\-458,-679,-417
+\\-488,449,543
+\\-626,468,-788
+\\338,-750,-386
+\\528,-832,-391
+\\562,-778,733
+\\-938,-730,414
+\\543,643,-506
+\\-524,371,-870
+\\407,773,750
+\\-104,29,83
+\\378,-903,-323
+\\-778,-728,485
+\\426,699,580
+\\-438,-605,-362
+\\-469,-447,-387
+\\509,732,623
+\\647,635,-688
+\\-868,-804,481
+\\614,-800,639
+\\595,780,-596
+\\
+\\--- scanner 4 ---
+\\727,592,562
+\\-293,-554,779
+\\441,611,-461
+\\-714,465,-776
+\\-743,427,-804
+\\-660,-479,-426
+\\832,-632,460
+\\927,-485,-438
+\\408,393,-506
+\\466,436,-512
+\\110,16,151
+\\-258,-428,682
+\\-393,719,612
+\\-211,-452,876
+\\808,-476,-593
+\\-575,615,604
+\\-485,667,467
+\\-680,325,-822
+\\-627,-443,-432
+\\872,-547,-609
+\\833,512,582
+\\807,604,487
+\\839,-516,451
+\\891,-625,532
+\\-652,-548,-490
+\\30,-46,-14
+;
+
+  var scanners = try calculateScanners(input);
+  defer { scanners.deinit(); }
+  const total = try numberOfBeacons(scanners);
+  try std.testing.expect(total == 79);
+  const max = maxManhattan(scanners);
+  try std.testing.expect(max == 3621);
 }
--- src/day20.zig
+++ src/day20.zig
@@ -1,18 +1,240 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
 const print = std.debug.print;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("../data/day20.txt");
 
-pub fn main() !void {
+const Image = struct {
+  key : std.StaticBitSet(512),
+  payload : std.DynamicBitSet,
+  infinitePixelIndex : usize,
+
+  pub fn deinit(me : *@This()) void {
+    me.payload.deinit();
+  }
+
+  pub fn initEmpty(allocator : *std.mem.Allocator) !@This() {
+    return Image {
+      .key = std.StaticBitSet(512).initEmpty(),
+      .payload = try std.DynamicBitSet.initEmpty(0, allocator),
+      .infinitePixelIndex = 0,
+    };
+  }
+
+  pub fn setKey(me : *@This(), key: []const u8) !void {
+    std.debug.assert(key.len == 512);
+
+    for (key) |c, i| {
+      if (c == '#') {
+        me.key.set(i);
+      } else {
+        // We set the initial infinite pixel index to any dark region of the key
+        // because we know from the input that the infinite region is initially
+        // all dark.
+        me.infinitePixelIndex = i;
+      }
+    }
+  }
+
+  pub fn addRow(me : *@This(), row : []const u8) !void {
+    const offset = me.payload.capacity();
+    try me.payload.resize(me.payload.capacity() + row.len, false);
+
+    for (row) |c, i| {
+      if (c == '#') {
+        me.payload.set(offset + i);
+      }
+    }
+  }
+
+  // Calculate whether a is in the range [offset..(offset + width))
+  fn inBounds(a : usize, offset : i32, width : usize) bool {
+    if (offset < 0) {
+      const poffset = @intCast(usize, -offset);
+      if (a < poffset) {
+        return false;
+      } else {
+        return (a - poffset) < width;
+      }
+    } else {
+      return (a + @intCast(usize, offset)) < width;
+    }
+  }
+
+  fn doOffset(a : usize, o : i32) usize {
+    if (o < 0) {
+      return a - @intCast(usize, -o);
+    } else {
+      return a + @intCast(usize, o);
+    }
+  }
+
+  fn lookupKeyIndex(me : *const @This(),
+    infinitePixel : bool,
+    newWidth : usize,
+    x : usize,
+    y : usize) usize {
+    var values = [9]bool {
+      infinitePixel, infinitePixel, infinitePixel,
+      infinitePixel, infinitePixel, infinitePixel,
+      infinitePixel, infinitePixel, infinitePixel};
+
+    const originalWidth = newWidth - 4;
+
+    var index : usize = 0;
+
+    var yOffset : i32 = -3;
     
+    while (yOffset <= -1) : (yOffset += 1) {
+     var xOffset : i32 = -3;
+    
+      while (xOffset <= -1) : (xOffset += 1) {
+        if (inBounds(x, xOffset, originalWidth) and inBounds(y, yOffset, originalWidth)) {
+          values[index] = me.payload.isSet(doOffset(y, yOffset) * originalWidth + doOffset(x, xOffset));
+        }
+
+        index += 1;
+      }
+    }
+
+    var result : usize = 0;
+    
+    for (values) |v| {
+      result <<= 1;
+      if (v) {
+        result |= 1;
+      }
+    }
+
+    return result;
+  }
+
+  pub fn dump(me : *const @This()) void {
+    const width = std.math.sqrt(me.payload.capacity());
+
+    var y : usize = 0;
+    while (y < width) : (y += 1) {
+      var x : usize = 0;
+      while (x < width) : (x += 1) {
+        const c : u8 = if (me.payload.isSet(y * width + x)) '#' else '.';
+        print("{c}", .{c});
+      }
+      print("\n", .{});
+    }
+  }
+
+  pub fn enhance(me : *@This()) !void {
+    // First thing we need to do is increase our image canvas to account for
+    // the new pixels.
+    const originalWidth = std.math.sqrt(me.payload.capacity());
+
+    // the 'interesting' region of the infinite image occurs 2 pixels in each
+    // direction beyond our original image.
+    const newWidth = 2 + originalWidth + 2;
+
+    const newSize = newWidth * newWidth;
+
+    const infinitePixel = me.key.isSet(me.infinitePixelIndex);
+
+    // Set the infinite pixel index. This pixel index is the value that the rest
+    // of the infinite image would be, assuming that it starts as dark pixels
+    // (as the problem described). 9 dark pixels will map to the 0'th key in the
+    // input (which cheekily in my input is #), and 9 light pixels will map to
+    // the last index (which cheekily in my input is 0). This means that the
+    // index of the infinite region can flip between lit/dark pixels on each
+    // enhance, gross!
+    me.infinitePixelIndex = if (infinitePixel) me.key.capacity() - 1 else 0;
+
+    const newInfinitePixel = me.key.isSet(me.infinitePixelIndex);
+
+    var new = if (newInfinitePixel) 
+      try std.DynamicBitSet.initFull(newSize, me.payload.allocator) else
+      try std.DynamicBitSet.initEmpty(newSize, me.payload.allocator);
+
+    {
+      var y : usize = 0;
+
+      while (y < newWidth) : (y += 1) {
+        var x : usize = 0;
+
+        while (x < newWidth) : (x += 1) {
+          const keyIndex = me.lookupKeyIndex(infinitePixel, newWidth, x, y);
+          const value = me.key.isSet(keyIndex);
+
+          new.setValue(y * newWidth + x, value);
+        }
+      }
+    }
+
+    me.payload.deinit();
+    me.payload = new;
+  }
+
+  pub fn count(me : *const @This()) usize {
+    std.debug.assert(!me.key.isSet(me.infinitePixelIndex));
+    return me.payload.count();
+  }
+};
+
+pub fn parseInput(input : []const u8) !Image {
+  var image = try Image.initEmpty(gpa);
+
+  var iterator = std.mem.tokenize(input, "\r\n");
+
+  try image.setKey(iterator.next().?);
+
+  while (iterator.next()) |line| {
+    try image.addRow(line);
+  }
+
+  return image;
+}
+
+pub fn main() !void {
+  var timer = try std.time.Timer.start();
+  var image = try parseInput(data);
+  defer { image.deinit(); }
+
+  var index : u32 = 0;
+
+  while (index < 50) : (index += 1) {
+    if (index == 2) {
+      print("🎁 Lit after 2: {}\n", .{image.count()});
+      print("Day 20 - part 01 took {:15}ns\n", .{timer.lap()});
+      timer.reset();
+    }
+
+    try image.enhance();
+  }
+
+  print("🎁 Lit after 50: {}\n", .{image.count()});
+  print("Day 20 - part 02 took {:15}ns\n", .{timer.lap()});
+  print("❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️\n", .{});
+}
+
+test "example" {
+  const input = \\..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+\\
+\\#..#.
+\\#....
+\\##..#
+\\..#..
+\\..###
+;
+
+  var image = try parseInput(input);
+  defer { image.deinit(); }
+
+  var index : u32 = 0;
+
+  while (index < 50) : (index += 1) {
+    if (index == 2) {
+      try std.testing.expect(image.count() == 35);
+    }
+    try image.enhance();
+  }
+
+  try std.testing.expect(image.count() == 3351);
 }
